b416ab8cd3f939e98f3dff8c176408ceb3a6fee7
Ethan Zhang <zhangzhonghao@mscsi.onmicrosoft.com>
2020-06-17 18:02:02 +0800

    CallCognitiveAPI

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 
-namespace ConsoleApp1
+namespace CallCognitiveAPI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string url = "http://localhost:7071/api/Function1";
-            string base64str = File.ReadAllText(@"E:\Repo\AudioBase64Converter\audio\testwebmbase64.txt");
+            string url = "https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=zh-CN&format=detailed";
+            string base64str = File.ReadAllText(@"E:\Repo\AudioBase64Converter\audio\convertwavbase64.txt");
             string responseContentTxt = @"E:\Repo\AudioBase64Converter\audio\responseContentTxt.txt";
 
 
             HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            //httpWebRequest.ContentType = contentType;
             httpWebRequest.Method = "POST";
-            httpWebRequest.Timeout = 20000;            
+            httpWebRequest.Accept = @"application/json;text/xml";
+            httpWebRequest.ProtocolVersion = HttpVersion.Version11;
+            httpWebRequest.Host = "eastus.stt.speech.microsoft.com";
+            httpWebRequest.ContentType = @"audio/wav; codecs=audio/pcm; samplerate=16000";
+            httpWebRequest.Headers["Ocp-Apim-Subscription-Key"] = "a7ab1a67c9544c529f877cabd75c67e6";
+            var s = httpWebRequest.GetRequestStream();
+            var strings = new StringContent(base64str);
 
             byte[] btBodys = Convert.FromBase64String(base64str);
             httpWebRequest.ContentLength = btBodys.Length;
@@ -25,7 +31,7 @@ namespace ConsoleApp1
             HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
             string responseContent = streamReader.ReadToEnd();
-            //File.WriteAllText(responseContentTxt, responseContent);
+            File.WriteAllText(responseContentTxt, responseContent);
             httpWebResponse.Close();
             streamReader.Close();
             httpWebRequest.Abort();
